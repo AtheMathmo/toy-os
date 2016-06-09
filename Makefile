1d63f6a1fb8f824c61fea523524219e1e8c843a3
James Lucas <LucasJ94@hotmail.co.uk>
2016-06-08 17:06:51 -0700

    Fixing the rust kernel!

--- Makefile
+++ Makefile
@@ -35,7 +35,7 @@ kernel: $(kernel)
 $(kernel): rust_kernel
 	@mkdir -p build
 	nasm asm/kernel_entry.asm -f elf -o $(kernel_entry)
-	ld --gc-sections -m elf_i386 -o $(kernel) -Ttext 0x1000 $(kernel_entry) $(rust_kernel)
+	ld -m elf_i386 -o $(kernel) -T linker.ld $(kernel_entry) $(rust_kernel)
 
 rust_kernel:
-	cargo rustc --manifest-path ./kernel/Cargo.toml --target $(target)
+	cargo rustc --manifest-path ./kernel/Cargo.toml --target $(target) -- -C relocation-model=static -O
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -3,24 +3,28 @@
 #![no_std]
 #![feature(asm)]
 
-//extern crate rlibc;
+extern crate rlibc;
 
 #[no_mangle] // ensure that this symbol is called `rust_main` in the output
+#[start]
 pub extern fn rust_main() {
-	let x : *mut u8 = 0xb8000 as *mut u8;
+	let buffer = (0xb8000 + 1988) as *mut _;
 
-	//let chars = [ 0x58, 0x59, 0x60 ];
-	unsafe {
-		// Set the value to 'X' (hex 0x58)
-		let y = &mut *(x);
+	let hello_world = b"Hello World!";
+	let colour_byte = 0x1f; // White foreground, blue background
 
-		// for c in chars.iter() {
-		// 	let z = 0x40;
-		// }
-
-		*y = 0x58;
-
+	let mut hello_coloured = [colour_byte; 24];
+	// Fill with colour
+	for (i, char_byte) in hello_world.into_iter().enumerate() {
+		hello_coloured[i * 2] = *char_byte
 	}
+
+	// Write the message
+	unsafe {
+		*buffer = hello_coloured
+	};
+
+	loop {}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
@@ -30,15 +34,9 @@ pub extern fn rust_main() {
 // Otherwise triple seg
 #[no_mangle]
 pub extern fn fmod() -> ! { 
-	unsafe {
-		asm!("int3");
-	}
 	loop {}
 }
 #[no_mangle]
 pub extern fn fmodf() -> ! { 
-	unsafe {
-		asm!("int3");
-	}
 	loop {}
 }
